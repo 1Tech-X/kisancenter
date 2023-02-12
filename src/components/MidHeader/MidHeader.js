c97b27ec1cedc3684c9ef2671212ac2c2fe3ca84
1Tech-X <prashantsrivastav36@gmail.com>
2023-02-12 14:11:05 -0800

    first commit kisan center

---
+++ src/components/MidHeader/MidHeader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+const MidHeader = () => {
+  return (
+    <div>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Row>
+            
+          </Row>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default MidHeader
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TopHeader from '../components/TopHeader/TopHeader'
+import MidHeader from '../components/MidHeader/MidHeader'
+import { Container } from 'react-bootstrap'
+import NavbarMenu from '../components/NavbarMenu/NavbarMenu'
+const Home = () => {
+  return (
+    <div>
+        <TopHeader />
+        <MidHeader />
+        <NavbarMenu />
+         <Container>
+        </Container>
+    </div>
+  )
+}
+
+export default Home
