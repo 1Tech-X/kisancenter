8b5628ba769f2702577535095e105e8a51638935
1Tech-X <prashantsrivastav36@gmail.com>
2023-02-13 06:04:35 +0530

    third commit

--- src/components/MidHeader/MidHeader.js
+++ src/components/MidHeader/MidHeader.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import { Navbar, Container, Nav, Row, Col,Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './MidHeader.css'
 const MidHeader = () => {
   return (
     <div>
-      <Navbar expand="lg" variant="light" bg="light">
+      <Navbar expand="lg" variant="light" bg="light" className='mid-header'>
         <Container>
           <Row>
-            
+            <Col md={3} sm={12}>
+              <div className='logo d-flex   justify-content-center'>
+                <Link to="/">
+                <Image src='images/logo.png'  />
+                </Link>
+              </div>
+              
+            </Col>
+            <Col md={9} sm={12} className="d-flex justify-content-end header-right" >
+             <div className='ad-banner float-right'>
+             <a href="#"><img src="https://via.placeholder.com/729x90" class="img-fluid" alt="" /></a>
+             </div>
+            </Col>
           </Row>
         </Container>
       </Navbar>
--- src/components/NavbarMenu/NavbarMenu.js
+++ src/components/NavbarMenu/NavbarMenu.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Container, Navbar, Nav,NavDropdown } from 'react-bootstrap'
+import { Container, Navbar, Nav,NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './NavbarMenu.css';
 const NavbarMenu = () => {
   return (
     <div>
@@ -8,19 +10,21 @@ const NavbarMenu = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Menu</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item as={Link} to="/">Action</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item as={Link} to="/">Something</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link as={Link} to="/">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/">Gallery</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
